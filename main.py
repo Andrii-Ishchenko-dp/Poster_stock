85354d808accc45612ef7ad8d92ab7d683231d64
Andrii-Ishchenko-dp <android428901@gmail.com>
2022-10-08 14:42:26 +0300

    Сделал выгрузку за период времени, оставил отдельный файл выгрузки за 1 день с себестоимостями и итоговой ценой

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ data_konec_vsei_vigruzki = input('Введите дату конца в форм
 data_end = data_start
 days = (int(data_konec_vsei_vigruzki) - int(data_start))+1
 itog=[]
-print(days)
+listofname=[]
 
 while int(data_konec_vsei_vigruzki)+1>int(data_end):
 
@@ -30,53 +30,50 @@ while int(data_konec_vsei_vigruzki)+1>int(data_end):
                   'cost_end': res['response'][count_ing]['cost_end']
             }
             itog.append(ingred)
+            if res['response'][count_ing]['ingredient_name'] not in listofname: #создал список уникальных названий ингредиентов
+                  listofname.append(res['response'][count_ing]['ingredient_name'])
             count_ing += 1
       data_end = str(int(data_end)+1)
 
+
 print(itog)
+print(listofname)
 
 dict_bez_0_ostatkov=[]
-kolichestvo_stolpcov = 1
+
 wb = xlwt.Workbook()
 ws = wb.add_sheet('Все склады')
 ws.write(0, 0, 'Название:')
-ws.write(0, 1, data_start)
+for i in range(len(listofname)): #выгрузил название в список
+      ws.write(i+1,0,listofname[i])
 
+kolichestvo_stolpcov = 1
+
+for i in itog:
+      if type(i) is str:
+            ws.write(0, kolichestvo_stolpcov, i)
+            kolichestvo_stolpcov+=1
+
+itog.pop(0)
+
+x=1
+y=1
+for i in range(len(listofname)):
+      for t in range(len(itog)):
+            if type(itog[t]) is str:
+                  x+=1
+                  continue
+            if listofname[i] == itog[t]['name']:
+                  ws.write(y,x,itog[t]['end'])
+      y+=1
+      x=1
+
+wb.save('{}-{}.xls'.format(data_start, data_konec_vsei_vigruzki))
 
 #Какие проблемы?
 # 1. избавиться от нулевых остатков
 # 2. Решить вопрос, если ингредиент заканчивается в выбранный период времени, чтобы не сыпалась выгрузка, так как съедут значения
 # 3. Подумать над выгрузкой не по столпцам, а по строкам.
-
-
-
-# def importexel(*kwargs):
-#
-#       wb = xlwt.Workbook()
-#       ws = wb.add_sheet('Все склады')
-#       ws.write(0, 0, 'Название:')
-#       ws.write(0, 1, data_end)
-#       ws.write(0, 2, 'Себестоимость:')
-#       ws.write(0, 3, 'Остаток в деньгах:')
-#       dict_bez_0_ostatkov = []
-#       for i in range(len(itog)):
-#             if itog[i]['end'] == 0:
-#                   continue
-#             dict_bez_0_ostatkov.append(itog[i])
-#
-#       for i in range(len(dict_bez_0_ostatkov)):
-#
-#             ws.write(1 + i, 0, dict_bez_0_ostatkov[i]['name'])
-#             ws.write(1 + i, 1, dict_bez_0_ostatkov[i]['end'])
-#             ws.write(1 + i, 2, dict_bez_0_ostatkov[i]['cost_end'])
-#             ws.write(1 + i, 3, xlwt.Formula("B{}*C{}".format(i+2,i+2)))
-#       ws.write(len(dict_bez_0_ostatkov)+1, 3, xlwt.Formula("SUM(D2:D{})".format(len(dict_bez_0_ostatkov)+1)))
-#       wb.save('{}-{}.xls'.format(data_start, data_end))
-#
-#
-# importexel(itog)
-
-# print(res)
 
 
 #Сделать выгрузку остатков по дням в эксель. Каждая вкладка будет отвечать за отдельный склад и одна вкладка с общей инфой
